a9048a152d29f1740840e6f73156f53cd31c5d50
atoool <scrapycoco.sam@gmail.com>
2022-03-29 21:03:36 +0530

    on click image show functionality integrated

--- assets/js/buttonClicks.js
+++ assets/js/buttonClicks.js
@@ -51,3 +51,69 @@ const expYear = dt + "+ Years of Experience";
 document.getElementById("expYear").innerText = expYear;
 
 //on project click view images
+
+function showPortFolioModal(i, j) {
+  let imgList = projects[i]?.list[j]?.img ?? [];
+  if (imgList && imgList?.length > 0) {
+    let imgListComp = '<div class="w3-content w3-display-container">';
+    const imgListHtml = document.getElementById("imglist112");
+    imgList?.map(
+      (k) =>
+        (imgListComp += `
+      <img class="mySlides" src="${baseURL + k}" style="width:100%">
+      `)
+    );
+    imgListComp += `
+    <div class="button-area" style="display: flex;justify-content: space-around;width: 100%;margin-top: 20px;">
+        <a class="rn-btn" onclick="plusDivs(-1)" href="javascript:void(0);"><span>&#10094;</span></a>
+        <a class="rn-btn" onclick="plusDivs(1)" href="javascript:void(0);"><span>&#10095;</span></a>
+    </div>
+    </div>`;
+    imgListHtml.innerHTML = imgListComp;
+    plusDivs(1);
+    $("#exampleModalCenter").modal("show");
+  }
+}
+//portfolio listing
+const portfolio = document.getElementById("portfolio112");
+let portfolioComp = "";
+projects.map((elem, i) => {
+  elem?.list?.map(
+    (itm, j) =>
+      (portfolioComp += `
+<div data-aos="fade-up" data-aos-delay="100" data-aos-once="true" class="col-lg-6 col-xl-4 col-md-6 col-12 mt--50 mt_md--30 mt_sm--30">
+                        <div class="rn-portfolio" data-toggle="modal" onclick="showPortFolioModal(${i},${j})"  >
+                            <div class="inner">
+                                <div class="thumbnail">
+                                    <a href="javascript:void(0);">
+                                        <img src=${
+                                          baseURL +
+                                          (itm?.img
+                                            ? itm?.img[0]
+                                            : "portfolio-03.jpg")
+                                        } alt="Personal Portfolio Images">
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <div class="category-info">
+                                        <div class="category-list">
+                                            <a href="javascript:void(0);">${
+                                              elem?.name
+                                            }</a>
+                                        </div>
+                                    </div>
+                                    <h4 class="title"><a href="javascript:void(0);">${
+                                      itm?.name
+                                    }<i class="feather-arrow-up-right"></i></a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+`)
+  );
+});
+portfolio.innerHTML = portfolioComp;
+// <div class="meta">
+//     <span><a href="javascript:void(0)"><i class="feather-heart"></i></a>
+//         600</span>
+// </div>
